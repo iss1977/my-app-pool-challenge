636cd5e7fe841f4603df02a3e15ddbc09a7cbfdf
iss1977 <ioan.szucs@gmail.com>
2020-11-01 22:17:04 +0100

    Working with charts

---
+++ src/components/PoolResult.vue
@@ -0,0 +1,126 @@
+<template>
+
+  <v-container>
+    <v-row class="text-center">
+        <v-col cols="12">
+            <h2>Hello from Result window</h2>
+            <p>I am {{username}}</p>
+
+            <v-progress-circular
+            :rotate="360"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="teal"
+            >
+            {{ value }}
+            </v-progress-circular>
+
+
+        </v-col>
+    </v-row>
+    <v-col>
+        <v-progress-linear
+            v-model="value"
+            height="25"
+        >
+            <strong>{{ Math.ceil(value) }}%</strong>
+        </v-progress-linear>
+    </v-col>
+  </v-container>
+
+
+
+</template>
+
+
+
+
+
+
+<script>
+export default {
+    name:"PoolResult",
+    props:{
+        username:{
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        favoriteTwoot(id){
+            this.$emit('favourite',id);
+        }
+    },
+    data(){
+        return {
+            value:12,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.twoot-item{
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border : 1px solid #DFE3E8;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.25s ease;
+
+    &:hover{
+        transform: rotate3d(1,1,1,45deg);
+        transform: scale(1.1,1.1);
+    }
+
+    .twoot-item__user{
+        font-weight: bold;
+    }
+
+} // end of .twoot-item
+
+
+
+.vote-window__main{
+
+    background-color: aquamarine;
+    padding: 1px;
+
+    .vote-window__question-label{
+        background-color: aliceblue;
+        opacity: 60%;
+        border-radius: 10px;
+        box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0);
+        margin : 20px;
+        border: 1px solid darkgray;
+
+    }
+
+    .vote-window__buttons-area{
+        margin: 10px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        
+
+        button{
+            width : 30%;
+            max-width: 300px;
+            min-width: max-content;
+            padding: 10px;
+            transition: all 0.3s cubic-bezier(.44,.57,.89,-0.21);
+
+            &:hover{
+                transform: scale(1.1,1.3);
+                opacity: 0.7;
+                border : 3px solid blueviolet;
+                padding:7px;
+            }
+        }
+    }
+
+}
+
+</style>
---
+++ src/components/Vote.vue
@@ -0,0 +1,104 @@
+<template>
+
+  
+    <v-row class="my-vote-window text-center">
+      <v-col cols="12">
+          <h2>Hello from Vote</h2>
+          <p>I am {{username}}</p>
+
+            <v-banner
+                elevation="4"
+                slot="icon"
+            >
+                <svg-icon slot ="icon" type="mdi" :path="path" ></svg-icon>
+
+                <h2>Corona safe?</h2>
+            </v-banner>
+
+      </v-col>
+    
+    
+      <v-col cols="12">
+            <v-card
+                color="#385F73"
+                dark
+            >
+                <v-card-title class="headline">
+                Unlimited music now
+                </v-card-title>
+
+                <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+                <v-card-actions>
+                <v-btn text>
+                    Listen Now
+                </v-btn>
+                </v-card-actions>
+            </v-card>
+            </v-col>  
+    
+
+
+    </v-row>
+
+    
+
+</template>
+
+
+
+
+
+
+<script>
+
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiAutoFix  } from '@mdi/js'
+
+
+
+export default {
+    name:"Vote",
+    components:{SvgIcon},
+    props:{
+        username:{
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        favoriteTwoot(id){
+            this.$emit('favourite',id);
+        }
+    },
+    data(){
+        return {
+            path:mdiAutoFix ,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-vote-window {
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border : 1px solid #DFE3E8;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.25s ease;
+
+    &:hover{
+        transform: rotate3d(1,1,1,45deg);
+        transform: scale(1.1,1.1);
+    }
+
+   
+
+} // end of .twoot-item
+
+
+
+
+</style>
